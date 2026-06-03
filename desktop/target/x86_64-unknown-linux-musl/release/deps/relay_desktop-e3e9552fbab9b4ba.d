cf213fb90aed0f9c5a99a2739186b753e54afea5
Abdk4Moura <cadaynstudio@gmail.com>
2026-06-03 09:19:53 +0000

    desktop: bump to 0.10.0 (cross-platform)

---
+++ desktop/target/x86_64-unknown-linux-musl/release/deps/relay_desktop-e3e9552fbab9b4ba.d
@@ -0,0 +1,7 @@
+/root/android-hid-input/desktop/target/x86_64-unknown-linux-musl/release/deps/relay_desktop-e3e9552fbab9b4ba.d: src/main.rs
+
+/root/android-hid-input/desktop/target/x86_64-unknown-linux-musl/release/deps/relay_desktop-e3e9552fbab9b4ba: src/main.rs
+
+src/main.rs:
+
+# env-dep:CARGO_PKG_VERSION=0.10.0
